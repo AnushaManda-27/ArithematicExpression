851df9fa14740ba23e4c8101322685925a70b9ae
AnushaManda-27 <anushamanda27@gmail.com>
2021-04-30 15:54:08 +0530

    Added computation to Arithematic Expression

--- ArithematicExpression.sh
+++ ArithematicExpression.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
 
 echo "Welcome to Arithematic Expression"
+read -p "Enter first number:" a
+read -p "Enter second number:" b
+read -p "Enter the third number:" c
 
+echo "$a,$b,$c"
+
+p=$(( a + b * c ))
+echo "p=$p"
