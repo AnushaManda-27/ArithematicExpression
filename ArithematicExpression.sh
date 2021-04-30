06ab41ecf845baa32b05c3e6032af5d0c1041bcc
AnushaManda-27 <anushamanda27@gmail.com>
2021-04-30 16:34:50 +0530

    Added computation

--- ArithematicExpression.sh
+++ ArithematicExpression.sh
@@ -17,3 +17,16 @@ s=$(( a % b + c ))
 echo "s=$s"
 arrayName=( $p $q $r $s )
 echo ${arrayName[@]}
+
+for ((i = 0; i<4; i++))
+do
+
+    for((j = 0; j<4-i-1; j++))
+    do
+
+        if [ ${arrayName[j]} -gt ${arrayName[$((j+1))]} ]
+        #@@ -43,4 +44,4 @@ do
+done
+
+echo "Array in sorted order :"
+echo ${[@]}
