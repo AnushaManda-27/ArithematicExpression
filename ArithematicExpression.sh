10328ce170a181b2fd32cebc93c38679a1b9a4ef
AnushaManda-27 <anushamanda27@gmail.com>
2021-04-30 16:45:46 +0530

    Added computation

--- ArithematicExpression.sh
+++ ArithematicExpression.sh
@@ -20,12 +20,35 @@ echo ${arrayName[@]}
 
 for ((i = 0; i<4; i++))
 do
+  for (( i = 0; i <4 ; i++ ))
+   do
+     for (( j =1; j <4; j++ ))
+      do
+        if [ ${arrayName[i]} -lt ${arrayName[j+1]} ]; then
+         temp=${arrayName[i]}
+          arrayName[$i]=${arrayName[j+1]}
+            arrayName[$j+1]=$temp
+         fi
+      done
+done
+
+echo "Sorted Computation for descending order"
+
+for (( i=0; i <4; i++ ))
+do
 
     for((j = 0; j<4-i-1; j++))
     do
 
         if [ ${arrayName[j]} -gt ${arrayName[$((j+1))]} ]
-        #@@ -43,4 +44,4 @@ do
+        then
+            # swap
+            temp=${arrayName[j]}
+            arrayName[$j]=${arrayName[$((j+1))]}  
+            arrayName[$((j+1))]=$temp
+        fi
+    done
+echo ${arrayName[$i]}
 done
 
 echo "Array in sorted order :"
